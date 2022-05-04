345580dbd7ce16ce087707e2469adbf0475abee9
Ricardo Ferreira <ricardofsouza@outlook.com>
2022-05-04 09:25:45 -0300

    limpar

--- receitas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from receitas.views import contato, home, sobre
-
-urlpatterns = [
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
-]
