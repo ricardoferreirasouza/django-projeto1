9d58aa08a73822c7d09bc96fec18baba8489ba6e
Ricardo Ferreira <ricardofsouza@outlook.com>
2022-05-06 16:11:25 -0300

    Urls, views e templates

--- receitas/views.py
+++ receitas/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'receitas/home.html', context={'name': 'Ricardo Ferreira',})
+    return render(request, 'receitas/pages/home.html', context={'name': 'Ricardo Ferreira',})
 
